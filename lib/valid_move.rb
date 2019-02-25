9eb85a154be9f7cea105f2e481d39929f249082b
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-25 15:29:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,11 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+   if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
+
+
+end
